e440343d70b04653a1449353824c274acafbcffe
fundamental <mark.d.mccurry@gmail.com>
2017-08-14 15:51:52 -0400

    Add Test Case

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,22 @@
 using TotalVariation
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+
+#Initial signals
+srand(0)
+g_truth = [ones(100); 5*ones(200); -10*ones(100)]
+noise   = randn(size(g_truth))
+mixed   = g_truth .+ noise
+
+
+#Denoising operation
+denoised      = tv(mixed, 10.0)
+
+#Results
+noise_before  = mean((mixed   .-g_truth).^2)
+noise_after   = mean((denoised.-g_truth).^2)
+
+println("Noise before = ", noise_before)
+println("Noise after  = ", noise_after)
+
+@test noise_before > noise_after
